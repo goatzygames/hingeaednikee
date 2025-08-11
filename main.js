6a674a0156976d99ae3239e702960e9a8b69dad2
goatzygames <7hamsterman7@gmail.com>
2025-08-11 11:33:33 +0300

    0.1

---
+++ main.js
@@ -0,0 +1,8 @@
+// main.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('cta');
+  btn.addEventListener('click', () => {
+    alert('Button Placeholder — toiming siia');
+  });
+});
